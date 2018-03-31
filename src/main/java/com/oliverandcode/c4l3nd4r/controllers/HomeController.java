e43139dd1c7f23d68257cb5fdcbac109c443fdec
orphanedcouplet <oliverdayers@gmail.com>
2018-03-31 15:46:00 -0500

    made it say hello world in bootrun

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/HomeController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/HomeController.java
@@ -2,13 +2,15 @@ package com.oliverandcode.c4l3nd4r.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "")
+    @ResponseBody
     public String index() {
-        return "index";
+        return "hello world";
     }
 
 }
