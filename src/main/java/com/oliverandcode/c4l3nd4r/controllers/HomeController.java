eb1ab8c66a3f88f318ff896d3d4c4a5dfff27598
orphanedcouplet <oliverdayers@gmail.com>
2018-04-01 14:52:25 -0500

    tried to make it say hello world with more sophisticated code but ran into a problem - page title says hello world but nothing displays in the body not even an error just a blank white page - is this a problem with my stylesheet? thymeleaf? compatibility between them? it can't be the controller code because the page title worked but i can't figure this out right now so i'm committing for now

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/HomeController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/HomeController.java
@@ -1,16 +1,16 @@
 package com.oliverandcode.c4l3nd4r.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "hello world";
+    public String index(Model model) {
+        model.addAttribute("title", "hello world");
+        return "index";
     }
 
 }
