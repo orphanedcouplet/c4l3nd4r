2145795d17f378c784b0e76d226d79732a193b16
orphanedcouplet <oliverdayers@gmail.com>
2018-04-13 14:52:51 -0500

    added annotations in controllers, edited routing a bit, began work on events/my-events template (index/home page maybe), added add-event template

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
@@ -1,15 +1,27 @@
 package com.oliverandcode.c4l3nd4r.controllers;
 
+import com.oliverandcode.c4l3nd4r.models.data.EventDao;
+import com.oliverandcode.c4l3nd4r.models.data.LocationDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "events")
 public class EventController {
 
-    @RequestMapping(value = "events")
+    @Autowired
+    private EventDao eventDao;
+
+    @Autowired
+    private LocationDao locationDao;
+
+    // request path: /events
+    @RequestMapping(value = "")
     public String index(Model model) {
         model.addAttribute("title", "My Events");
+        model.addAttribute("events", eventDao.findAll());
         return "events/my-events";
     }
 
--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/LocationController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/LocationController.java
@@ -1,15 +1,22 @@
 package com.oliverandcode.c4l3nd4r.controllers;
 
+import com.oliverandcode.c4l3nd4r.models.data.LocationDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "locations")
 public class LocationController {
 
-    @RequestMapping(value = "locations")
+    @Autowired
+    private LocationDao locationDao;
+
+    @RequestMapping(value = "")
     public String index(Model model) {
         model.addAttribute("title", "My Locations");
+        model.addAttribute("locations", locationDao.findAll());
         return "locations/my-locations";
     }
 
