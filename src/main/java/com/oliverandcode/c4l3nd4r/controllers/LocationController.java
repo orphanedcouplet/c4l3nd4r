5ba0d2e367cab360c671606087fca5cc98cd52a5
orphanedcouplet <oliverdayers@gmail.com>
2018-04-17 23:32:33 -0500

    made add-location page work! added nav bars to add-event template

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/LocationController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/LocationController.java
@@ -1,10 +1,16 @@
 package com.oliverandcode.c4l3nd4r.controllers;
 
+import com.oliverandcode.c4l3nd4r.models.Location;
 import com.oliverandcode.c4l3nd4r.models.data.LocationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "locations")
@@ -20,5 +26,25 @@ public class LocationController {
         model.addAttribute("locations", locationDao.findAll());
         return "locations/my-locations";
     }
+
+    @RequestMapping(value = "add-location", method = RequestMethod.GET)
+    public String displayAddLocationForm(Model model) {
+        model.addAttribute("title", "New Location");
+        model.addAttribute(new Location());
+        return "locations/add-location";
+    }
+
+    @RequestMapping(value = "add-location", method = RequestMethod.POST)
+    public String processAddLocationForm(@ModelAttribute @Valid Location newLocation,
+                                         Errors errors,
+                                         Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "New Location");
+            return "locations/add-location";
+        }
+        locationDao.save(newLocation);
+        return "redirect:";
+    }
+
 
 }
