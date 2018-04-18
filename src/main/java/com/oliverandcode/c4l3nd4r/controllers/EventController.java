9529375b184ada2c381a4f53e8f93404dd411d3d
orphanedcouplet <oliverdayers@gmail.com>
2018-04-17 20:34:50 -0500

    added add methods (get and post) to EventController - not hooked up to templates yet

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
@@ -1,11 +1,19 @@
 package com.oliverandcode.c4l3nd4r.controllers;
 
+import com.oliverandcode.c4l3nd4r.models.Event;
+import com.oliverandcode.c4l3nd4r.models.Location;
 import com.oliverandcode.c4l3nd4r.models.data.EventDao;
 import com.oliverandcode.c4l3nd4r.models.data.LocationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "events")
@@ -24,5 +32,32 @@ public class EventController {
         model.addAttribute("events", eventDao.findAll());
         return "events/my-events";
     }
+
+    // request path: /events/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddEventForm(Model model) {
+        model.addAttribute("title", "New Event");
+        model.addAttribute(new Event());
+        model.addAttribute("locations", locationDao.findAll());
+        return "events/add-event";
+    }
+
+    // request path: /events/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddEventForm(@ModelAttribute @Valid Event newEvent,
+                                      Errors errors,
+                                      @RequestParam int locationId,
+                                      Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "New Event");
+            return "events/add-event";
+        }
+
+        Location location = locationDao.findOne(locationId);
+        newEvent.setLocation(location);
+        eventDao.save(newEvent);
+        return "redirect:";
+    }
+
 
 }
