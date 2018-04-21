88dc916cdf7ce67ef8fc5d0bfdb7ca2840c4d864
orphanedcouplet <oliverdayers@gmail.com>
2018-04-21 10:00:21 -0500

    update works PARTLY -- just need to fix the dateAndTime parts of the form

--- src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
+++ src/main/java/com/oliverandcode/c4l3nd4r/controllers/EventController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Calendar;
@@ -68,6 +65,27 @@ public class EventController {
 
         eventDao.save(newEvent);
 
+        return "redirect:";
+    }
+
+    // request path: /events/edit-event/{#}
+    @RequestMapping(value = "edit-event/{eventId}", method = RequestMethod.GET)
+    public String displayEditEventForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = eventDao.findOne(eventId);
+        model.addAttribute("title", "Edit Event");
+        model.addAttribute("event", eventToEdit);
+        return "events/edit-event";
+    }
+
+    // request path: /events/edit-event/{#}
+    @RequestMapping(value = "edit-event/{eventId}", method = RequestMethod.POST)
+    public String processEditEventForm(int eventId, String title, String description, Calendar dateAndTime, Location location) {
+        Event eventToEdit = eventDao.findOne(eventId);
+        eventToEdit.setTitle(title);
+        eventToEdit.setDescription(description);
+        eventToEdit.setDateAndTime(dateAndTime);
+        eventToEdit.setLocation(location);
+        eventDao.save(eventToEdit);
         return "redirect:";
     }
 
